df44e5e16915a7829c94d3df644e796d1c2120c1
unknown <mildredmeyah@gmail.com>
2022-06-22 15:39:29 +0200

    budget

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import AddItem from './components/addItems';
+
+
+function App() {
+  return (
+    <div className="App">
+     <AddItem/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/addItems.js
@@ -0,0 +1,19 @@
+function AddItem (){
+    return(
+        <div class="container">
+            
+            <input placeholder="Enter Item"/><br></br>
+            <input placeholder="Enter Amount"/><br></br>
+            <select>
+                <option>Income</option><br></br>
+                <option>Expence</option>
+            </select>
+            <br></br>
+            <button>add</button>
+    </div>
+
+    )
+    
+    
+}
+export default AddItem;
